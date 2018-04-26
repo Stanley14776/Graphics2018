c7d50aa2d69b66514dc9335abfb3e1f3f67cf479
Stanley14776 <email_id>
2018-04-26 20:34:51 +0100

    Specular sufaces added

--- raytracer.cpp
+++ raytracer.cpp
@@ -5,6 +5,7 @@
 #include "TestModelH.h"
 #include <stdint.h>
 #include <limits>
+#include "Material.h"
 
 using namespace std;
 using glm::vec3;
@@ -14,8 +15,8 @@ using glm::mat4;
 
 
 // #define SCREEN_WIDTH 208
-#define SCREEN_HEIGHT 500
-#define SCREEN_WIDTH 500
+#define SCREEN_HEIGHT 100
+#define SCREEN_WIDTH 100
 // #define SCREEN_HEIGHT 164
 #define FULLSCREEN_MODE false
 vector<Triangle> triangles;
@@ -34,6 +35,7 @@ struct Intersection
   vec4 position;
   float distance;
   int triangleIndex;
+  Material material;
 };
 
 /* ----------------------------------------------------------------------------*/
@@ -48,12 +50,14 @@ vec3 DirectLight( const Intersection& i );
 bool SphereIntersection(vec4 start, vec4 dir,
                          const vector<Sphere>& spheres,
                          Intersection& closestIntersection );
+vec4 ReflectedRay(vec4 normal, vec4 dir);
+vec3 getReflectedColour(Intersection& intersectionReflection , vec4 dir );
 
 int main( int argc, char* argv[] )
 {
 
   LoadTestModel( triangles );
-  spheres.push_back(Sphere(vec4(470,50,100, 1),50,vec3(1,0.5,0)));
+  spheres.push_back(Sphere(vec4(400,50,100, 1),50,vec3(1,0.5,0), Diffuse));
   float L = 555;
   spheres[0].centre *= 2/L;
   spheres[0].centre -= vec4(1,1,1,1);
@@ -91,10 +95,25 @@ void Draw(screen* screen)
         if(ClosestIntersection(cameraPos, d, triangles, intersection)){
 
           int i = intersection.triangleIndex;
-					vec3 illumination =DirectLight(intersection);
-          vec3 colour = illumination+triangles[i].color;
-					PutPixelSDL(screen, x, y, colour*indirectLight);
-          // std::cout << "illum x: " <<illumination.x << std::endl;
+          Intersection intersection2 = intersection;
+					// vec3 illumination =DirectLight(intersection);
+          vec3 colour = triangles[i].color;
+
+          if(intersection.material == Specular){ // && maxBounce > 0
+            vec4 reflection = ReflectedRay(triangles[i].normal, d);
+            // std::cout << "colour: ( "<<illumination.x
+            //  <<", "<<illumination.y<<", "<<illumination.z<<")"<< std::endl;
+            //  std::cout<< "i index: " << i<< std::endl;
+
+            colour = getReflectedColour(intersection2 , reflection );
+            // vec3 illumination =DirectLight(intersection);
+
+          }
+          vec3 illumination =DirectLight(intersection2);
+          vec3 finalColour = illumination+colour;
+          if(colour == vec3(0,0,0)) finalColour = colour;
+          // if(colour == vec3(0,0,0)) finalColour = colour;
+					PutPixelSDL(screen, x, y, finalColour*indirectLight);
           }
 
         else PutPixelSDL(screen, x, y, vec3(0,0,0));
@@ -147,6 +166,7 @@ bool ClosestIntersection(vec4 start,
  bool doesIntersect = false;
  closestIntersection.distance = m;
  for(int i=0; i<triangles.size(); i++){
+
     vec4 v0 = triangles[i].v0;
     vec4 v1 = triangles[i].v1;
     vec4 v2 = triangles[i].v2;
@@ -165,18 +185,23 @@ bool ClosestIntersection(vec4 start,
     v = x.z;
 
     vec4 r = (start+t*dir);
+    float distance = glm::distance(start,r);
+    // if(t > 0){ // for Specular
     //Check if intersection occured with triangle
+    // if((0<=u) && (0<=v) && (u+v<=1)){
     if((0<=u) && (0<=v) && (u+v<=1) && (0 <= t)){
       doesIntersect = true;
       //Check if intersection is new min
       float currentMin = closestIntersection.distance;
-      float distance = glm::distance(start,r);
+      // float distance = glm::distance(start,r);
       if(distance < currentMin){
         closestIntersection.position = r;
         closestIntersection.distance = distance;
         closestIntersection.triangleIndex = i;
+        closestIntersection.material = triangles[i].material;
       }
     }
+  // }
   }
   return doesIntersect;
 
@@ -194,6 +219,7 @@ vec3 DirectLight( const Intersection& i ){
   if(i.triangleIndex == 100){ //if sphere
     Sphere s = spheres[0];
     n = glm::normalize((i.position-s.centre)/s.radius);
+    spheres[0].normal = n;
     colour = s.color;
     // std::cout << "n " <<n.y << std::endl;
     if(ClosestIntersection(i.position+vec4(0.1,0.1,0.1,0.1)*r,r,triangles,intersection)){
@@ -284,12 +310,79 @@ bool SphereIntersection(vec4 start, vec4 dir,
       if(distance < currentMin){
         closestIntersection.position = iPoint;
         closestIntersection.distance = distance;
-        closestIntersection.triangleIndex = 100; // large value indicating sphere
+        closestIntersection.triangleIndex = 100;  // large value indicating sphere
+        closestIntersection.material = s.material;
       }
   }
 
   return doesIntersect;
 }
+
+vec4 ReflectedRay(vec4 normal, vec4 dir){
+  vec4 reflected = dir - (float(2)*(normal*dir)*normal);
+
+  return reflected;
+}
+
+
+vec3 getReflectedColour(Intersection& iReflection , vec4 dir ){
+    vec3 colour;
+    vec4 start = iReflection.position;
+    bool doesIntersect = false;
+    int p = iReflection.triangleIndex;
+    for(int i=0; i<triangles.size(); i++){
+      if((i != iReflection.triangleIndex) && (i != p) ){
+       vec4 v0 = triangles[i].v0;
+       vec4 v1 = triangles[i].v1;
+       vec4 v2 = triangles[i].v2;
+
+       vec3 e1 = vec3(v1.x-v0.x,v1.y-v0.y,v1.z-v0.z);
+       vec3 e2 = vec3(v2.x-v0.x,v2.y-v0.y,v2.z-v0.z);
+       vec3 b = vec3(start.x-v0.x,start.y-v0.y,start.z-v0.z);
+
+       vec3 d = vec3(dir.x, dir.y, dir.z);
+       mat3 A( -d, e1, e2 );
+       vec3 x = glm::inverse( A ) * b;
+
+       float t,u,v;
+       t = x.x;
+       u = x.y;
+       v = x.z;
+
+       vec4 r = (start+t*dir);
+       float distance = glm::distance(start,r);
+       if((0<=u) && (0<=v) && (u+v<=1) && (0 < t)){
+         //Check if intersection is new min
+         float currentMin = iReflection.distance;
+
+         if(distance < currentMin){
+           iReflection.position = r;
+           iReflection.distance = distance;
+           iReflection.triangleIndex = i;
+           // std::cout << "Index Colour: "<< i << endl;
+           // iReflection.material = triangles[i].material;
+           colour = triangles[i].color;
+           // colour = vec3(1,0,0);
+           doesIntersect = true;
+         }
+       }
+     }
+   }
+
+  if(SphereIntersection(start,  dir, spheres, iReflection )){
+    colour = spheres[0].color;
+  }
+
+
+
+
+   if(!doesIntersect) colour = vec3(0,0,0);
+    return colour;
+}
+
+
+
+
 
 
 
